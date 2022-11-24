a26cac0027bb81e5d5c9b483055d0957f4ba0668
LIka1599 <99115297+LIka1599@users.noreply.github.com>
2022-11-25 01:34:10 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
 $(function () {
+  //плавный скролл
+  $(".footer__top-btn").on("click", function (e) {
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $(anchor).offset().top - 60
+    }, 800);
+  });
+
   $('.popular__slider').slick({
     arrows: false,
     infinite: true,
     slidesToShow: 3,
     draggable: true,
-    responsive: [
-      {
-        breakpoint: 1100,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 750,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-    ]
+    // responsive: [
+    //   {
+    //     breakpoint: 1100,
+    //     settings: {
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 750,
+    //     settings: {
+    //       slidesToShow: 2
+    //     }
+    //   },
+    // ]
   })
-  $('.team__slider-prev').on('click', function (e) {
-    e.preventDefault()
-    $('.team__slider').slick('slickPrev')
-  })
-  $('.team__slider-next').on('click', function (e) {
-    e.preventDefault()
-    $('.team__slider').slick('slickNext')
-  })
+  // $('.team__slider-prev').on('click', function (e) {
+  //   e.preventDefault()
+  //   $('.team__slider').slick('slickPrev')
+  // })
+  // $('.team__slider-next').on('click', function (e) {
+  //   e.preventDefault()
+  //   $('.team__slider').slick('slickNext')
+  // })
 
   $('.partners__slider').slick({
     arrows: false,
@@ -48,13 +57,44 @@ $(function () {
       },
     ]
   })
-  $('.team__slider-prev').on('click', function (e) {
+  $('.partners__prev').on('click', function (e) {
     e.preventDefault()
-    $('.team__slider').slick('slickPrev')
+    $('.partners__slider').slick('slickPrev')
   })
-  $('.team__slider-next').on('click', function (e) {
+  $('.partners__next').on('click', function (e) {
     e.preventDefault()
-    $('.team__slider').slick('slickNext')
+    $('.partners__slider').slick('slickNext')
+  })
+
+  $('.comments__slider').slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    draggable: true,
+  })
+  $('.comments__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.comments__slider').slick('slickPrev')
+  })
+  $('.comments__next').on('click', function (e) {
+    e.preventDefault()
+    $('.comments__slider').slick('slickNext')
+  })
+
+
+  $('.burger-menu, .owerlay, .close').on('click', function (e) {
+    e.preventDefault()
+    if ($('.header__menu').hasClass('header__menu--open')) {
+      $('.header__menu').removeClass('header__menu--open');
+      $('.owerlay').removeClass('owerlay--show');
+      //$('.burger-menu').removeClass('burger-menu--open');
+    }
+    else {
+      $('.header__menu').addClass('header__menu--open');
+      $('.owerlay').addClass('owerlay--show');
+      //$('.burger-menu').addClass('burger-menu--open');
+    }
+
   })
 
 })
