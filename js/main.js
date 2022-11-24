0c6ead13abfa54b5a4632cb35921a2dcdeeead0c
LIka1599 <99115297+LIka1599@users.noreply.github.com>
2022-11-24 16:29:54 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function () {
+  $('.popular__slider').slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+    ]
+  })
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
+
+  $('.partners__slider').slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+    ]
+  })
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
+
+})
